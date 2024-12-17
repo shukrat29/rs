44ca402d8cc5dcbb442e253ab08006bb602a7733
Shukra Tamang <shukratamang.npl@gmail.com>
2024-12-17 21:57:09 +1100

    complete Hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,13 @@
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+
 const HomePage = () => {
-  return <div className="text-2xl">HomePage</div>;
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
+  );
 };
 
 export default HomePage;
